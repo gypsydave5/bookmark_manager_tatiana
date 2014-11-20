59e65247650ec602366664e7bfbd38d06ab1988f
David Wickes <dave.wickes@gmail.com>
2014-11-20 16:37:29 +0000

    refactors the controllers out into seperate files

--- app/server.rb
+++ app/server.rb
@@ -1,12 +1,12 @@
 require 'data_mapper'
 require 'sinatra/base'
 require 'rack-flash'
-require './lib/link' # this needs to be done after datamapper is initialised
-require './lib/tag'
-require './lib/user'
+
+Dir["./lib/*"].each {|model| require model}
+Dir[ "./controllers/*" ].each {|controller| require controller}
+
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
-
 
 class BookmarkManager < Sinatra::Base
 	enable :sessions
@@ -16,46 +16,5 @@ class BookmarkManager < Sinatra::Base
 	set :views, File.join(root, '..', '/app/views')
 	set :public_folder, File.join(root, '..', '/app/public')
 
-	get '/' do
-		@links = Link.all
-		erb :index
-	end
-
-	post '/links' do
-		url = params["url"]
-		title = params["title"]
-		tags = params["tags"].split(" ").map do |tag|
-			Tag.first_or_create(:text => tag)
-		end
-		Link.create(:url => url, :title => title, :tags => tags)
-		redirect to('/')
-	end
-
-	get '/tags/:text' do
-		tag = Tag.first(:text => params[:text])
-		@links = tag ? tag.links : []
-		erb :index
-	end
-
-	get '/users/new' do
-		@user = User.new
-		erb :"users/new"
-	end
-
-	post '/users' do
-		@user = User.new(:email => params[:email],
-										 :password => params[:password],
-										 :password_confirmation => params[:password_confirmation])
-		if @user.save
-			session[:user_id] = @user.id
-			redirect to ('/')
-		else
-			flash.now[:errors] = @user.errors.full_messages
-			erb :"users/new"
-		end
-	end
-
-
-	# start the server if ruby file executed directly
 	run! if app_file == $0
 end
